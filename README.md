de71d7c30ec805552b266efbb9b252ef31db44d5
ArnabChakraborty123 <chakrabortyarnab0001@gmail.com>
2024-07-13 00:44:38 +0530

    toggle vol added

--- README.md
+++ README.md
@@ -1,4 +1,61 @@
 
 # Vocalixa
-A text to speech converter using Amazon Polly
+A text-to-speech converter using Amazon Polly
+
 ![Screenshot 2024-07-03 214212](https://github.com/ArnabChakraborty123/Vocalixa/assets/98545064/f64789b1-cbda-4b55-b233-b79c1e2f3381)
+![Screenshot 2024-07-07 114246](https://github.com/ArnabChakraborty123/Vocalixa/assets/98545064/3b470943-8b28-4ed5-82ae-b338e6d4c899)
+
+## Overview
+Vocalixa is a text-to-speech converter built using Next.js and Amazon Polly. The application leverages AWS IAM for secure access and features an outstanding user interface.
+
+## Features
+- Convert text to speech using Amazon Polly.
+- Real-time conversion with playback support.
+- Intuitive and visually appealing user interface.
+
+## Technologies Used
+- **Next.js**: A React framework for building server-side rendered applications.
+- **Amazon Polly**: A service that turns text into lifelike speech.
+- **AWS IAM**: Securely manage access to AWS services and resources.
+
+## Getting Started
+
+### Prerequisites
+- Node.js
+- AWS account with access to Amazon Polly and IAM permissions
+
+### Installation
+
+1. **Clone the repository:**
+   ```bash
+   git clone https://github.com/ArnabChakraborty123/Vocalixa.git
+   cd Vocalixa
+   ```
+
+2. **Install dependencies:**
+   ```bash
+   npm install
+   ```
+
+3. **Set up your environment variables:**
+   Create a `.env` file in the root directory and add your AWS credentials and region.
+   ```
+   NEXT_PUBLIC_REGION=your-aws-region
+   NEXT_PUBLIC_CLIENT_ID=your-aws-access-key-id
+   NEXT_PUBLIC_SECRET_ACCESS_KEY=your-aws-secret-access-key
+   ```
+
+4. **Run the application:**
+   ```bash
+   npm run dev
+   ```
+
+   Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+## Usage
+
+1. **Enter the text** you want to convert to speech in the provided textarea.
+2. **Click the convert button** to generate the speech.
+3. **Listen** to the generated audio directly within the application.
+4.**Download** the audio directly in your pc
+Feel free to adjust any part of this README as necessary!
--- src/app/components/audioplayer.js
+++ src/app/components/audioplayer.js
@@ -1,8 +1,5 @@
-
 import React, { useState, useEffect, createRef } from "react";
-import { Play } from "lucide-react";
-import { Pause } from "lucide-react";
-import { Download } from "lucide-react";
+import { Play, Pause, Download, Volume1Icon, VolumeX} from "lucide-react";
 
 const Audioplayer = ({ audioFile, isTextEntered }) => {
   const audioRef = React.createRef();
@@ -10,6 +7,8 @@ const Audioplayer = ({ audioFile, isTextEntered }) => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [currentTime, setCurrentTime] = useState(0);
   const [duration, setDuration] = useState(0);
+  const [showSpeedOptions, setShowSpeedOptions] = useState(false);
+  const [isMuted, setIsMuted] = useState(false); // new state for mute/unmute
 
   useEffect(() => {
     if (audioFile) {
@@ -40,7 +39,16 @@ const Audioplayer = ({ audioFile, isTextEntered }) => {
     }
     setIsPlaying(!isPlaying);
   };
-
+  const toggleMute = () => {
+    const audio = audioRef.current;
+    if (isMuted) {
+      audio.muted = false;
+      setIsMuted(false);
+    } else {
+      audio.muted = true;
+      setIsMuted(true);
+    }
+  };
   const downloadAudio = () => {
     const audioBlob = new Blob([audioFile], { type: "audio/mpeg" });
     const audioURL = URL.createObjectURL(audioBlob);
@@ -50,42 +58,77 @@ const Audioplayer = ({ audioFile, isTextEntered }) => {
     link.click();
   };
 
+  const changeSpeed = (speed) => {
+    audioRef.current.playbackRate = speed;
+    setSpeed(speed);
+    setShowSpeedOptions(false);
+  };
+
   return (
     <div className="flex flex-col justify-center items-center ">
-      <div className="w-3/4 bg-gray-200 rounded-full h-2.5 mb-4 overflow-hidden">
-        <div
-          className="bg-gradient-to-r from-purple-500 to-red-700 h-2.5"
-          ref={progressBarRef}
-          style={{ width: `${(currentTime / duration) * 100}%` }}
-        />
+      <div className="flex items-center w-3/4">
+        <button
+          className="ml-2 bg-gray-200 hover:bg-gradient-to-r from-purple-500 to-red-700 rounded-full p-2"
+          onClick={toggleMute}
+        >
+          {isMuted ? (
+            <VolumeX size={24} fill="#FFFFFF" />
+          ) : (
+            <Volume1Icon size={24} fill="#FFFFFF" />
+          )}
+        </button>
+        <div className="w-full bg-gray-200 rounded-full h-2.5 mb-4 overflow-hidden">
+          <div
+            className="bg-gradient-to-r from-purple-500 to-red-700 h-2.5"
+            ref={progressBarRef}
+            style={{ width: `${(currentTime / duration) * 100}%` }}
+          />
+        </div>
       </div>
       <div className="flex gap-6">
-      <button
-        onClick={() => togglePlay()}
-        className={`rounded-full p-4 ${
-          isTextEntered
-            ? "bg-purple-500 hover:bg-red-700 text-white font-bold p-4 mt-4 rounded-full flex items-center justify-center shadow-lg transition duration-300 ease-in-out transform hover:scale-110"
-            : "bg-gray-300 text-gray-500 cursor-not-allowed font-bold p-4 mt-4 rounded-full flex items-center justify-center shadow-lg transition duration-300 ease-in-out transform hover:scale-110"
-        }`}
-        style={{ width: "50px", height: "50px" }}
-        disabled={!isTextEntered}
-      >
-        <audio ref={(element) => { audioRef.current = element; }} />
+        <div className="relative inline-block">
+          <button
+            onClick={() => togglePlay()}
+            className={`rounded-full p-4 ${
+              isTextEntered
+                ? "bg-purple-500 hover:bg-red-700 text-white font-bold p-4 mt-4 rounded-full flex items-center justify-center shadow-lg transition duration-300 ease-in-out transform hover:scale-110"
+                : "bg-gray-300 text-gray-500 cursor-not-allowed font-bold p-4 mt-4 rounded-full flex items-center justify-center shadow-lg transition duration-300 ease-in-out transform hover:scale-110"
+            }`}
+            style={{ width: "50px", height: "50px" }}
+            disabled={!isTextEntered}
+            onMouseEnter={() => setShowSpeedOptions(true)}
+            onMouseLeave={() => setShowSpeedOptions(false)}
+          >
+            <audio
+              ref={(element) => {
+                audioRef.current = element;
+              }}
+            />
 
-        {/* <audio ref={audioRef} /> */}
-        {isPlaying ? (
-          <Pause className="text-black" />
-        ) : (
-          <Play className="text-black" />
-        )}
-      </button>
-      <button
-        className="bg-purple-500 hover:bg-red-700 text-white font-bold p-4 mt-4 rounded-full flex items-center justify-center shadow-lg transition duration-300 ease-in-out transform hover:scale-110"
-        style={{ width: "50px", height: "50px" }}
-        onClick={downloadAudio}
-      >
-        <Download size={24} />
-      </button>
+            {isPlaying ? (
+              <Pause className="text-black" />
+            ) : (
+              <Play className="text-black" />
+            )}
+          </button>
+          {showSpeedOptions && (
+            <div className="absolute top-16 left-1/2 transform -translate-x-1/2 bg-white border rounded shadow-lg z-10">
+              <ul className="p-2">
+                <li className="cursor-pointer p-1 hover:bg-gray-200" onClick={() => changeSpeed(0.5)}>0.5x</li>
+                <li className="cursor-pointer p-1 hover:bg-gray-200" onClick={() => changeSpeed(1)}>1x</li>
+                <li className="cursor-pointer p-1 hover:bg-gray-200" onClick={() => changeSpeed(1.5)}>1.5x</li>
+                <li className="cursor-pointer p-1 hover:bg-gray-200" onClick={() => changeSpeed(2)}>2x</li>
+              </ul>
+            </div>
+          )}
+        </div>
+        <button
+          className="bg-purple-500 hover:bg-red-700 text-white font-bold p-4 mt-4 rounded-full flex items-center justify-center shadow-lg transition duration-300 ease-in-out transform hover:scale-110"
+          style={{ width: "50px", height: "50px" }}
+          onClick={downloadAudio}
+        >
+          <Download size={24} />
+        </button>
       </div>
     </div>
   );
